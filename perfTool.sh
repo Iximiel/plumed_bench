1d617c8a4c16dda35274fbd2598222884ff0f046
Daniele Rapetti <5535617+Iximiel@users.noreply.github.com>
2023-12-18 17:45:47 +0100

    less errorprone perfTool.sh

--- perfTool.sh
+++ perfTool.sh
@@ -6,18 +6,25 @@ colors=
 #git clone https://github.com/brendangregg/FlameGraph
 flameDir=$HOME/scratch/repos/FlameGraph
 
-dirtype=plumed15_12
-Extra=Nopbc
+#18_12 has an extra stopwatch
+dirtype=plumed18_12
+Extra="NoPbc-stopwatch"
 # colors="--colors io"
 for name in master htt; do
   fname=${name}${dirtype}${Extra}
   echo "perfing \"./plumedProfiler \$HOME/scratch/installs/${name}${dirtype}/lib/libplumedKernel.so\""
-  perf record -F99 -g ./plumedProfiler $HOME/scratch/installs/${name}${dirtype}/lib/libplumedKernel.so >"${fname}.out"
-  perf script >"${fname}.perf"
+  # -g Enables call-graph (stack chain/backtrace) recording for both kernel space and user space.
+  # -o, --output= Output file name.
+  # -F, --freq= Profile at this frequency. Use max to use the currently maximum allowed frequency, i.e. the value in the kernel.perf_event_max_sample_rate sysctl. Will throttle down to the currently maximum allowed frequency. See --strict-freq.
+  perf record -F99 -g --output="${fname}.data" \
+    ./plumedProfiler $HOME/scratch/installs/${name}${dirtype}/lib/libplumedKernel.so >"${fname}.out"
+  # -i, --input= Input file name. (default: perf.data unless stdin is a fifo)
+  perf script --input="${fname}.data" >"${fname}.perf"
 
-  ${flameDir}/stackcollapse-perf.pl "${fname}.perf" >"${fname}.folded"
-  ${flameDir}/flamegraph.pl "${fname}.folded" --cp ${colors} --subtitle ${fname} >"${fname}.svg"
+  "${flameDir}"/stackcollapse-perf.pl "${fname}.perf" >"${fname}.folded"
+  "${flameDir}"/flamegraph.pl "${fname}.folded" --cp ${colors} --subtitle ${fname} >"${fname}.svg"
+  "${flameDir}"/flamegraph.pl "${fname}.folded" --cp ${colors} --subtitle ${fname} --flamechart >"${fname}.flamechart.svg"
   colors="--colors chain"
 done
-${flameDir}/difffolded.pl master${dirtype}${Extra}.folded htt${dirtype}${Extra}.folded |
-  ${flameDir}/flamegraph.pl --subtitle "diff master-htt ${dirtype}${Extra}" >diff${dirtype}${Extra}.svg
+"${flameDir}"/difffolded.pl master${dirtype}${Extra}.folded htt${dirtype}${Extra}.folded |
+  "${flameDir}"/flamegraph.pl --subtitle "diff master-htt ${dirtype}${Extra}" >diff${dirtype}${Extra}.svg
